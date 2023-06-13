46c004b74384ab294c4dfa848bfe92e83e01162e
Ambrgna <mittens.pritchett@gmail.com>
2023-06-13 10:25:46 -0500

    set up anguar

---
+++ restaurant-fullstack/src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { NavComponent } from './components/nav/nav.component';
+import { MenuComponent } from './components/menu/menu.component';
+import { MenuItemComponent } from './components/menu-item/menu-item.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    FooterComponent,
+    NavComponent,
+    MenuComponent,
+    MenuItemComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
